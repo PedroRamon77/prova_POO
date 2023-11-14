a1b79b1b10591421ef1afd57da1812d920ec4ab1
PedroRamon77 <115299054+PedroRamon77@users.noreply.github.com>
2023-11-14 15:05:14 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package clinica;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Agendamedica  a = new Agendamedica();
+		a.agendar("Joao pedro", "14");
+		
+		Marcacaohorario m = new Marcacaohorario();
+
+
+	}
+
+}
